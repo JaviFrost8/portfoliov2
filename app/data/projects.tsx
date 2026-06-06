e45d8b2e76488c6130ffbafbbdf7abc608e2a612
Javi <Jjaviergonzalez23@gmail.com>
2026-06-06 11:58:42 +0200

    Fix: Proyectos nuevos

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -1,7 +1,7 @@
-import citalia from '@/public/citalia.png';
-import ecommerce from '@/public/ecommerce.png';
+import marea from '@/public/marea-alta.webp';
+import collector from '@/public/collector.webp';
 import { StaticImageData } from 'next/image';
-import portfolio from '@/public/portfolio.png';
+import rafaello from '@/public/rafaello.webp';
 import vicio from '@/public/vicio.png';
 
 interface PropsProjects {
@@ -17,55 +17,54 @@ interface PropsProjects {
 export const projects: PropsProjects[] = [
   {
     id: 1,
-    nombre: 'Citalia',
+    nombre: 'Marea alta',
     descripcion:
-      'Aplicación web para descubrir y compartir citas célebres, desarrollada con React y Firebase para la gestión de datos',
-    imagen: citalia,
+      'Desarrollo de una web moderna para restaurante enfocada en mejorar la presencia online, facilitar reservas y ofrecer una experiencia visual atractiva tanto en móvil como en escritorio.',
+    imagen: marea,
     caracteristicas: [
-      'Firebase',
-      'Autenticación de usuarios',
-      'Interacción de usuarios',
+      'Diseño responsive',
+      'Sistema de reservas',
+      'Optimización SEO',
     ],
-    demo: 'https://citalia-project.vercel.app/',
-    codigo: 'https://github.com/JaviFrost8/citalia',
+    demo: 'https://marea-alta-orpin.vercel.app/',
+    codigo: 'https://github.com/JaviFrost8/marea-alta',
   },
   {
     id: 2,
+    nombre: 'The Collector',
+    descripcion:
+      'Aplicación web para gestionar colecciones personales de películas. Cada usuario puede crear su propia biblioteca, guardar títulos, organizarlos y acceder a sus datos desde cualquier dispositivo gracias a Firebase.',
+    imagen: collector,
+    caracteristicas: [
+      'Firebase',
+      'Autenticación de usuarios',
+      'Base de datos en tiempo real',
+    ],
+    demo: 'https://thecollectorv2.vercel.app/',
+    codigo: 'https://github.com/JaviFrost8/theCollectorV2',
+  },
+  {
+    id: 3,
     nombre: 'Vicio',
     descripcion:
-      'Aplicación para concienciar sobre el deterioro y la destrucción que producen las drogas',
+      'Aplicación interactiva para concienciar sobre los efectos del consumo de drogas. Mediante decisiones y distintos escenarios, el usuario experimenta cómo las adicciones afectan a la salud, las relaciones personales y la calidad de vida, mostrando su impacto progresivo de forma visual y educativa.',
     imagen: vicio,
     caracteristicas: ['Diseño Responsive', 'Estado global', 'React Router'],
     demo: 'https://vicioapp-z9b8.vercel.app/',
     codigo: 'https://github.com/JaviFrost8/vicio',
   },
   {
-    id: 3,
-    nombre: 'Eva Stettic',
-    descripcion:
-      'E-commerce de estética y bienestar con carrito de compras gestionado mediante estado global usando Context API',
-    imagen: ecommerce,
-    caracteristicas: [
-      'Estado Global',
-      'Lógica de carrito',
-      'Arquitectura de componentes',
-    ],
-    demo: 'https://stetticfinish.vercel.app/',
-    codigo: 'https://github.com/JaviFrost8/stettic',
-  },
-  {
     id: 4,
-    nombre: 'Portfolio',
+    nombre: 'Rafaello3D',
     descripcion:
-      'Portfolio personal desarrollado con Next.js con diseño responsive, rendimiento y buenas prácticas de desarrollo',
-    imagen: portfolio,
+      'Sitio web desarrollado para una empresa especializada en impresión 3D y fabricación personalizada. El proyecto está enfocado en mostrar sus servicios, trabajos realizados y facilitar el contacto con potenciales clientes mediante una experiencia moderna y responsive.',
+    imagen: rafaello,
     caracteristicas: [
-      'NextJS',
-      'Typescript',
-      'SEO',
-      'Optimización',
-      'Diseño Responsive',
+      'Galería de imagenes',
+      'Formulario de contacto',
+      'Optimización SEO',
     ],
-    codigo: 'https://github.com/JaviFrost8/portfoliov2',
+    demo: 'https://rafaello3d.es/',
+    codigo: 'https://github.com/JaviFrost8/rafaello3d',
   },
 ];
